70b9fb5a305f78daa32f3945ab6930cc762c97fc
vanti113 <56195006+vanti113@users.noreply.github.com>
2020-10-26 23:52:02 +0900

    make it better

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,8 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userObj, setUserObj] = useState(null);
   //위의 userObj는 모든 컴포넌트에서 활용될 가능성이 있기 때문에 항상 탑레벨에서 정의되어야 한다. 그래야 프롭스로 전달..
+  //이것은 굉장히 중요한 이유인데, 탑레벨의 객체인 userObj가 하위레벨에서 변경되거나 갱신되면 모든 소속된 컴포넌트가 리렌더링이 된다. 이것이 하나의 소스를 통일하여
+  //사용해야 하는 이유이다. 가령 프로필을 변경후 리렌더링을 시켜야 한다고 가정했을때, userObj의 내용을 갱신하는 것이 매우 중요한 키 포인트가 된다.
 
   useEffect(() => {
     //
@@ -18,7 +20,12 @@ function App() {
       //onAUthStateChanged 는 로그인 혹은 로그아웃이 콜백을 만들거다. 그 콜백의 결과가 user객체
       if (user) {
         setIsLoggedIn(true);
-        setUserObj(user);
+        /*  setUserObj(user); // user객체는 엄청나게 크다. 따라서 아래의 리프레시유저 함수가 userObj를 임의로 리프레시 해도, 다 읽히지 않을수가 있다. ㄸ라서 필요한 부분만 가져오는게 더 좋다. */
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => user.updateProfile(args),
+        });
       } else {
         setIsLoggedIn(false);
       }
@@ -26,10 +33,25 @@ function App() {
     });
   }, []);
 
+  const refreshUser = () => {
+    //이 메소드의 목적은 하위레벨에서 userObj와 관련된 리렌더링의 트리거가 되게 하기 위함이다. 프롭스로 전달될 예정.
+    const user = authService.currentUser;
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: (args) => user.updateProfile(args),
+    }); //여기서 문제가 발생되는데, 리액트가 너무 큰 오브젝트를 다루면 내부적으로 에러가 발생할수가 있다.
+    //따라서,
+  };
+
   return (
     <>
       {init ? (
-        <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} />
+        <AppRouter
+          isLoggedIn={isLoggedIn}
+          userObj={userObj}
+          refreshUser={refreshUser}
+        />
       ) : (
         "Initializing...."
       )}
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,15 +1,60 @@
-import { authService } from "fBase";
-import React from "react";
+import { authService, dbService } from "fBase";
+import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
 
-const Profile = () => {
+const Profile = ({ userObj, refreshUser }) => {
   let history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
+
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection("nweets") //콜렉션의 이름
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createAt") //정렬에는 DB의 색인이 필요하다, 즉 인덱스가 필요한데...파이어베이스는 NOSQL이다보니 직접 색인등록을 해 주어야 한다.
+      .get();
+    console.log(nweets.docs.map((doc) => doc.data()));
+  };
+  /* 위의 메소드는 내 정보를 어떻게 필터링하는가에 대한 함수이다. firestore에 관한 인스턴스인 dbService는 내가 올린 데이터의 콜렉션을 제공한다.
+콜렉션 안에는 콜렉션이름을 지정하여야 한다. 그 이름은 내가 파이어베이스에 설정한 콜렉션네임이다. 이게 콜렉션패스 된다. 그 다음, where메소드를 사용하는데,
+쉽게 생각해서 콜렉션 안의 필드를 참조한다고 생각하자. 콜렉션 안의 creatorId 가 == 즉, 같은가...무엇과? 지금 이 컴포넌트가 프롭스로 받고 있는 userObj의 uid, 
+즉 지금 로그인 해 있는 유저와 같은가, 에 대해 쿼리하고, 그 다음 get()으로 문서를 다운로드 하여 nweets에 데이터가 들어간다. 그 데이터는 도큐먼트속성이 있기 때문에 
+docs프로퍼티 안에서 map을 통해 각각의 객체에 data()함수를 적용하여 실제로 사용할수 있는 객체로 반환된다.(map은forEach와 달라서 리턴 값이 있다.)
+*/
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+    refreshUser();
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+        //photoUrl을 업로드 하는 건 숙제로
+      });
+    }
+  };
+  useEffect(() => {
+    getMyNweets();
+  }, []);
+
   return (
     <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Display Name"
+          onChange={onChange}
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
